ae327b392e07ca5ddc3f5534de16a3c55fe6a759
hacker123-star <haseenakumari858@gmail.com>
2025-12-22 16:47:49 +0530

    Update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+export const API_BASE_URL = 
+  import.meta.env.VITE_API_BASE_URL || 
+  "https://klearnstudio-backend.onrender.com";
